7757e5901c3fbdf4e2a69b5426b46a8cff5c8f29
hdorottya <husztidorottya@gmail.com>
2017-05-01 21:43:14 +0200

    added some unit tests for basic functionality

--- src/test/java/org/noorganization/instalist/server/model/UnitTest.java
+++ src/test/java/org/noorganization/instalist/server/model/UnitTest.java
@@ -1,7 +1,65 @@
 package org.noorganization.instalist.server.model;
 
-/**
- * Created by Huszti József on 2017.05.01..
- */
+
+import org.junit.Before;
+import org.junit.Test;
+import org.noorganization.instalist.server.model.Ingredient;
+import org.noorganization.instalist.server.model.Product;
+import org.noorganization.instalist.server.model.Recipe;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+import static org.junit.Assert.*;
 public class UnitTest {
+    private Unit unit = new Unit();
+
+    private Set<Product> set = new HashSet<Product>();
+
+    @Before
+    public void setValues() throws Exception{
+        unit.setId(1);
+
+        Product i1 = new Product();
+        Product i2 = new Product();
+        set.add(i1);
+        set.add(i2);
+
+        unit.setProducts(set);
+    }
+
+    @Test
+    // testing getId method of Unit
+    public void getId() throws Exception{
+        int id = unit.getId();
+        assertEquals(1,id);
+    }
+
+    @Test
+    // testing setId method of Unit
+    public void setId() throws Exception{
+        unit.setId(5);
+
+        int id = unit.getId();
+        assertEquals(5, id);
+    }
+
+    @Test
+    // testing widthId of Unit
+    public void withId() throws Exception{
+        Unit un = unit.withId(10);
+
+        assertEquals(10, un.getId());
+    }
+
+    @Test
+    public void getProducts() throws Exception{
+        set = unit.getProducts();
+        int num = set.size();
+
+        assertEquals(2, num);
+    }
+
 }
